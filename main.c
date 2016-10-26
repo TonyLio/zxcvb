dee04c0f2326b66200d3a7d20f8eccdb84f5311c
TonyLio <sdv349653@yahoo.com.tw>
2016-10-26 16:14:00 +0800

    購買錢
    購買錢

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+  
+int m,n,d;
+scanf("%d",&m);
+scanf("%d",&n); 
+if(m<n)printf("n%d",m-n);
+if(m>n)
+{
+  
+  printf("y");
+         
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  system("PAUSE");	
+  return 0;
+}
